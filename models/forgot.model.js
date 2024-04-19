9abb73d8c1c0b88727b8785797462c1942cb7f13
tuanminh9704 <132085614+tuanminh9704@users.noreply.github.com>
2024-04-19 22:34:34 +0700

    Tạo collection fotgot-password

---
+++ models/forgot.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const forgotPasswordSchema = new mongoose.Schema({
+    email: String,
+    otp: String,
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    expiresAt: {
+        type: Date,
+        default: () => Date.now() + 3600000 // Expiry time: 1 hour from createdAt
+    }
+});
+
+// Đặt chỉ số TTL cho trường 'expiresAt' (đơn vị là giây)
+forgotPasswordSchema.index({ expiresAt: 1 }, { expireAfterSeconds: 30 });
+
+const ForgotPassword = mongoose.model(
+    "ForgotPassword",
+    forgotPasswordSchema,
+    "forgot-password"
+  );
+  
+  module.exports = ForgotPassword;
