fd943f8fb39096f522782260348fdcbb0ec7b7eb
tuanminh9704 <132085614+tuanminh9704@users.noreply.github.com>
2024-05-15 18:02:32 +0700

    Chèn icon vào đoạn chat

--- controllers/client/chat.controller.js
+++ controllers/client/chat.controller.js
@@ -1,41 +1,41 @@
 const User = require("../../models/user.model");
 const Chat = require("../../models/chat.model");
 
-module.exports.index = async(req, res) => {
+module.exports.index = async (req, res) => {
     const userId = res.locals.user.id;
     const fullName = res.locals.user.fullName;
-
-    // console.log(userId);
-    _io.once('connection',  (socket) => {
-        socket.on("CLIENT_SEND_MESSAGE_TO_SERVER", async (content) => {
-            // console.log(content);
-            const chat = new Chat({
-                user_id: userId,
-                content: content,
-            })
-            // console.log(content);
-            await chat.save();
-            socket.emit("SEVER_RETURN_MESSAGE_TO_CLIENT", {
-                userId: userId,
-                fullName: fullName,
-                content: content
-            })
-        })
+  
+    // SocketIO
+    _io.once("connection", (socket) => {
+      // Người dùng gửi tin nhắn lên server
+      socket.on("CLIENT_SEND_MESSAGE", async (content) => {
+        const chat = new Chat({
+          user_id: userId,
+          content: content
+        });
+        await chat.save();
+        _io.emit("SERVER_SEND_MESSAGE", {
+          userId: userId,
+          fullName: fullName,
+          content: content
+        });
       });
-
-    const chats = await Chat.find({
-        deleted: false,
-    })
-
-    for (const chat of chats) {
-        const infoUser = await User.findOne({
-            _id: chat.user_id,
-        }).select("fullName")
-        // console.log(inforUser);
-        chat.infoUser = infoUser;
-    }
-    res.render("client/pages/chat/index", {
-        pageTitle: "Chat",
-        chats: chats,
     });
-}
+    // End SocketIO
+    // Lấy data từ database
+    const chats = await Chat.find({
+      deleted: false
+    });
+    for (const chat of chats) {
+      const infoUser = await User.findOne({
+        _id: chat.user_id
+      }).select("fullName");
+      chat.infoUser = infoUser;
+    }
+    // console.log(chats);
+    // Hết Lấy data từ database
+    res.render("client/pages/chat/index", {
+      pageTitle: "Chat",
+      chats: chats
+    });
+  };
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,55 +1,74 @@
-// Client send message
-const formData = document.querySelector(".chat .inner-form");
-// console.log(formData);
-if(formData){
-    const inputContent = formData.querySelector("input[name='content']");
-    formData.addEventListener("submit", (event) => {
-        event.preventDefault();
-        const content = inputContent.value;
-        if(content){
-            socket.emit("CLIENT_SEND_MESSAGE_TO_SERVER", content);
-            content.value = "";
-        }
-    });
+// CLIENT_SEND_MESSAGE
+const formSendData = document.querySelector(".chat .inner-form");
+if(formSendData) {
+  const inputContent = formSendData.querySelector("input[name='content']");
+  formSendData.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const content = inputContent.value;
+    if(content) {
+      socket.emit("CLIENT_SEND_MESSAGE", content);
+      inputContent.value = "";
+    }
+  });
+}
+// End CLIENT_SEND_MESSAGE
+
+// SERVER_SEND_MESSAGE
+socket.on("SERVER_SEND_MESSAGE", (data) => {
+  const body = document.querySelector(".chat .inner-body");
+  const myId = document.querySelector("[my-id]").getAttribute("my-id");
+
+  const div = document.createElement("div");
+  let htmlFullName = "";
+
+  if(myId != data.userId) {
+    div.classList.add("inner-incoming");
+    htmlFullName = `<div class="inner-name">${data.fullName}</div>`;
+  } else {
+    div.classList.add("inner-outgoing");
+  }
+
+  div.innerHTML = `
+    ${htmlFullName}
+    <div class="inner-content">${data.content}</div>
+  `;
+
+  body.appendChild(div);
+  body.scrollTop = body.scrollHeight;
+})
+// End SERVER_SEND_MESSAGE
+
+// Scroll Chat To Bottom
+const bodyChat = document.querySelector(".chat .inner-body");
+if(bodyChat) {
+  bodyChat.scrollTop = bodyChat.scrollHeight;
 }
 
-// End Client send message
 
+// Emoji 
 
-// Server return message
+const buttonIcon = document.querySelector(".chat .button-icon");
+buttonIcon.addEventListener("click", () => {
+  const emoji = document.querySelector("emoji-picker");
+  // console.log(emoji.classList[1]);
 
-socket.on("SEVER_RETURN_MESSAGE_TO_CLIENT", (data) => {
-    // console.log(data);
-    const body = document.querySelector(".chat .inner-body");
-    const myId = document.querySelector("[my-id]").getAttribute("my-id");
-    // console.log(myId);
-    // console.log(body);
-    const div = document.createElement("div");
-    let htmlFullname = "";
-    if(data.userId != myId){
-        div.classList.add("inner-incoming");
-        htmlFullname = `<div class="inner-name">${data.fullName}</div>`;
-    }
-    else{
-        div.classList.add("inner-outgoing")
-    }
-    div.innerHTML = `
-        ${htmlFullname}
-        <div class="inner-content">${data.content}</div>
-    `;
-    console.log(div);
-    body.appendChild(div);
-    body.scrollTop = body.scrollHeight;
+  if(emoji.classList[1] === "d-none"){
+    emoji.classList.remove("d-none");
+  }
+  else{
+    emoji.classList.add("d-none");
+  }
 })
 
-// End server return message
 
+const emojiPicker = document.querySelector('emoji-picker');
 
-// Scroll bottom
+emojiPicker.addEventListener('emoji-click', event => {
+  const iconEmoji = event.detail.unicode;
+  const inputContent = document.querySelector(".chat input[name='content']");
+  // console.log(inputContent);
+  // console.log(iconEmoji);
+  inputContent.value = inputContent.value + iconEmoji;
+});
 
-const bodyChat = document.querySelector(".chat .inner-body");
-if(bodyChat){
-    bodyChat.scrollTop = bodyChat.scrollHeight;
-}
-
-// End scroll bottom
+// End Emoji
