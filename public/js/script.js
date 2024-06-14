e4455a6c43b010a29d65d3f6fdddaed193d20d1d
tuanminh9704 <132085614+tuanminh9704@users.noreply.github.com>
2024-06-14 09:29:17 +0700

    Hiển thị form tạo mới bài viết

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,20 @@
 // console.log("OK");
+// Hiển thị form tạo mới bài viết
+const innerContent = document.querySelector(".inner-content");
+
+console.log(innerContent);
+if(innerContent) {
+    innerContent.addEventListener("click", () => {
+        const innerFormCreatePost = document.querySelector(".inner-form-create-post");
+        // console.log(innerFormCreatePost);
+        innerFormCreatePost.classList.remove("d-none");
+        const buttonCloseForm = innerFormCreatePost.querySelector(".button-close-form");
+        if(buttonCloseForm){
+            buttonCloseForm.addEventListener("click", () => {
+                innerFormCreatePost.classList.add("d-none");
+            })
+        }
+    })
+}
+
+// End Hiển thị form bài viết
