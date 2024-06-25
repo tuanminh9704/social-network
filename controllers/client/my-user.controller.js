cee5ac346cecbd0bbcdd41989a2ec9861bcf27a2
tuanminh9704 <132085614+tuanminh9704@users.noreply.github.com>
2024-06-26 00:02:17 +0700

    Lấy data gửi thật trang cá nhân

--- controllers/client/my-user.controller.js
+++ controllers/client/my-user.controller.js
@@ -1,3 +1,23 @@
-module.exports.index = (req, res) => {
-    res.render("client/pages/my-user/index");
+const User = require("../../models/user.model");
+const Post = require("../../models/post.model");
+
+module.exports.index = async (req, res) => {
+    const myUserId = res.locals.user.id;
+    // console.log(myUserId);
+    const myUser = await User.findOne({
+        _id: myUserId,
+        deleted: false,
+    }).select("-password, -token");
+
+    const posts = await Post.find({
+        user_id: myUserId,
+        deleted: false,
+    })
+
+    // console.log(myUser);
+    res.render("client/pages/my-user/index", {
+        pageTitle: `${myUser.fullName}`,
+        myUser: myUser,
+        posts: posts
+    });
 }
